382a911063e739c4359440efb8d2b2a0ba6f1870
mjn <mjn298@gmail.com>
2020-07-06 10:26:30 -0400

    stuff

---
+++ src/main/scala/com/example/IotApp.scala
@@ -0,0 +1,9 @@
+package com.example
+
+import akka.actor.typed.ActorSystem
+
+object IotApp {
+  def main(args: Array[String]): Unit = {
+    ActorSystem[Nothing](IotSupervisor(), "iot-system")
+  }
+}
---
+++ src/main/scala/com/example/IotSupervisor.scala
@@ -0,0 +1,24 @@
+package com.example
+
+import akka.actor.typed.{Behavior, PostStop, Signal}
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+
+
+object IotSupervisor {
+  def apply(): Behavior[Nothing] =
+    Behaviors.setup[Nothing](context => new IotSupervisor(context))
+}
+
+class IotSupervisor(context: ActorContext[Nothing]) extends AbstractBehavior[Nothing](context) {
+  context.log.info("IoT Application Started")
+
+  override def onMessage(msg: Nothing): Behavior[Nothing] = {
+    Behaviors.unhandled
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Nothing]] = {
+    case PostStop =>
+      context.log.info("Iot Application Stopped")
+      this
+  }
+}
